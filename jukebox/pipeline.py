eb3cadbf7c8f41ea392e81f641d51a9e16ebafa7
zivepstein <isthisabookclub@gmail.com>
2021-04-09 18:15:21 -0400

    update sheet

--- jukebox/pipeline.py
+++ jukebox/pipeline.py
@@ -23,20 +23,21 @@ sheet_instance = sheet.get_worksheet(0)
 records_data = sheet_instance.get_all_records()
 
 records_df = pd.DataFrame.from_dict(records_data)
-params = ['model','levels','model','audio_file','prompt_length_in_seconds',	'sample_length_in_seconds',	'total_sample_length_in_seconds','sr','n_samples','hop_fraction','artist','genre','temp','lyrics']
-commands_to_run = []
-for i,r in records_df[records_df['to_run']==1].iterrows():
-	name =  "{}_{}_{}".format(r['audio_file'].split('/')[-1].split('.')[0].replace(" ",""), r['artist'].replace(" ",""),r['genre'].replace(" ",""))
-	print(name)
-	command = ['--name={}'.format(name)]
-	for param,value in r.items():
-		if param in params:
-			if isinstance(value, str) and " " in value:
-				command.append("--{}='{}'".format(param,value))
-			else:
-				command.append("--{}={}".format(param,value))
-	commands_to_run.append("python jukebox/sample.py " + " ".join(command))
-
-for command_to_run in commands_to_run:
-	print('launched "{}" at {}'.format(command_to_run, datetime.now().strftime("%H:%M:%S %Y")) )
-	os.system(command_to_run)
+hyperparameters = ['model','levels','model','audio_file','prompt_length_in_seconds',	'sample_length_in_seconds',	'total_sample_length_in_seconds','sr','n_samples','hop_fraction','artist','genre','temp','lyrics']
+for i,r in records_df.iterrows():
+	print(i)
+	if r['to_run']==1:
+		name =  "{}_{}_{}".format(r['audio_file'].split('/')[-1].split('.')[0].replace(" ",""), r['artist'].replace(" ",""),r['genre'].replace(" ",""))
+		print(name)
+		command = ['--name={}'.format(name)]
+		for param,value in r.items():
+			if param in hyperparameters:
+				if isinstance(value, str) and " " in value:
+					command.append("--{}='{}'".format(param,value))
+				else:
+					command.append("--{}={}".format(param,value))
+		command_to_run = "python jukebox/sample.py " + " ".join(command)
+		print('{} launched "{}" at {}'.format(i, command_to_run, datetime.now().strftime("%H:%M:%S %Y")) )
+		os.system(command_to_run)
+		sheet_instance.update_cell(i+2, 2, 0)
+		#upload file to drive
